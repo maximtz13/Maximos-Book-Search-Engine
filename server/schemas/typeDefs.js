13ac1301f0d696e6e2071d53dc2f91855ef02c15
Maximoo <maximtz99@gmail.com>
2022-11-30 20:27:33 -0600

    updated typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,16 +5,41 @@ const typeDefs = gql`
     _id: ID
     username: String
     email: String
+    bookCount: Int
+    savedBooks: [Book]
   }
+
+  input savedBooksInput {
+  authors: Array
+  description: String
+  title: String
+  bookId: String
+  image: String
+  link: String
+  }
+
+  type Book {
+  bookId: String
+  authors: Array
+  description: String
+  title: String
+  image: String
+  link: String
+  }
+
   type Auth {
     token: ID
     user: User
   }
+
   type Query {
-    user(username: String!): User
+    me: User
   }
+
   type Mutation {
+    login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    savedBooks(input: [savedBooksInput!]!) User
   }
 `;
 
